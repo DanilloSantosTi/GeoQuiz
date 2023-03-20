5e8a8a93259a8e66e026f7ff8fda49c6976e5829
DanilloSantosTi <danillomdsti@gmail.com>
2023-03-20 15:36:58 -0300

    [master] feature - create button next and business rule.

--- app/src/main/java/com/example/geoquiz/MainActivity.kt
+++ app/src/main/java/com/example/geoquiz/MainActivity.kt
@@ -1,32 +1,59 @@
 package com.example.geoquiz
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
-import android.widget.Button
-import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.example.geoquiz.databinding.ActivityMainBinding
 import com.google.android.material.snackbar.Snackbar
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var trueButton: Button
-    private lateinit var falseButton: Button
+    private lateinit var binding: ActivityMainBinding
+
+    private val questionBank = listOf(
+        Question(R.string.question_australia, true),
+        Question(R.string.question_oceans, true),
+        Question(R.string.question_mideast, false),
+        Question(R.string.question_africa, false),
+        Question(R.string.question_americas, true),
+        Question(R.string.question_asia, true)
+    )
+
+    private var currentIndex = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        updateQuestion()
 
-        trueButton = findViewById(R.id.true_button)
-        falseButton = findViewById(R.id.false_button)
+        binding.trueButton.setOnClickListener {
+            checkAnswer(true)
+        }
 
+        binding.falseButton.setOnClickListener {
+            checkAnswer(false)
+        }
+
+        binding.nextButton.setOnClickListener {
+            currentIndex = (currentIndex + 1) % questionBank.size
+            updateQuestion()
+        }
+    }
+
+    private fun updateQuestion() {
+        val questionTextResId = questionBank[currentIndex].textResId
+        binding.questionTextView.setText(questionTextResId)
+    }
+
+    private fun checkAnswer(useAnswer: Boolean) {
+        val correctAnswer = questionBank[currentIndex].answer
         val contextView = findViewById<View>(android.R.id.content)
 
-        trueButton.setOnClickListener {
-            Snackbar.make(contextView, R.string.correct_response, Snackbar.LENGTH_SHORT).show()
-        }
+        val messageId =
+            if (correctAnswer == useAnswer) R.string.correct_response
+            else R.string.incorrect_response
 
-        falseButton.setOnClickListener {
-            Toast.makeText(this, R.string.incorrect_response, Toast.LENGTH_SHORT).show()
-        }
+        Snackbar.make(contextView, messageId, Snackbar.LENGTH_SHORT).show()
     }
 }
