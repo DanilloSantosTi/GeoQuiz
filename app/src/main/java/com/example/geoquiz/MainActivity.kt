5342158cb77741b41339fcf6bff06bd0a61786ce
DanilloSantosTi <danillomdsti@gmail.com>
2023-03-23 17:09:29 -0300

    [master] feature - correct bug

--- app/src/main/java/com/example/geoquiz/MainActivity.kt
+++ app/src/main/java/com/example/geoquiz/MainActivity.kt
@@ -1,131 +1,97 @@
 package com.example.geoquiz
 
 import android.os.Bundle
-import android.util.Log
 import android.view.View
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import com.example.geoquiz.databinding.ActivityMainBinding
 import com.google.android.material.snackbar.Snackbar
 
-private const val TAG = "MainActivity"
-
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
     private val quizViewModel: QuizViewModel by viewModels()
-    private val userCorrectAnswer = mutableMapOf<String, String>()
-
-    private val questionBank = listOf(
-        Question(R.string.question_australia, true),
-        Question(R.string.question_oceans, true),
-        Question(R.string.question_mideast, false),
-        Question(R.string.question_africa, false),
-        Question(R.string.question_americas, true),
-        Question(R.string.question_asia, true)
-    )
-
-    private var currentIndex = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        Log.d(TAG, "onCreate(Bundle?) called")
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        Log.d(TAG, "Got a QuizViewModel: $quizViewModel")
         updateQuestion()
 
         binding.trueButton.setOnClickListener {
             checkAnswer(true)
-            disableButtons()
+            disableOrEnableButton(false)
         }
 
         binding.falseButton.setOnClickListener {
             checkAnswer(false)
-            disableButtons()
+            disableOrEnableButton(false)
         }
 
         binding.nextButton.setOnClickListener {
-            currentIndex = (currentIndex + 1) % questionBank.size
-            updateQuestion()
-            enableButtons()
+            val contextView = findViewById<View>(android.R.id.content)
+
+            if (quizViewModel.lastQuestion) {
+                disableOrEnableButton(buttonsResponseAnswer = false, nextAndPrevButtons = false)
+                Snackbar.make(
+                    contextView,
+                    quizViewModel.userPercentageCorrect(),
+                    Snackbar.LENGTH_INDEFINITE
+                )
+                    .setAction(R.string.Exit_button) {
+                        quizViewModel.resetCurrentIndex()
+                        updateQuestion()
+                        disableOrEnableButton(
+                            buttonsResponseAnswer = true,
+                            nextAndPrevButtons = true
+                        )
+                    }
+                    .show()
+            } else {
+                quizViewModel.moveToNext()
+                updateQuestion()
+                disableOrEnableButton(true)
+            }
         }
 
         binding.previousButton.setOnClickListener {
-            if (currentIndex != 0) {
-                currentIndex = (currentIndex - 1) % questionBank.size
+            if (quizViewModel.getCurrentIndex != 0) {
+                quizViewModel.previousQuestion()
                 updateQuestion()
-                disableButtons()
+                disableOrEnableButton(false)
             }
         }
     }
 
-    override fun onStart() {
-        super.onStart()
-        Log.d(TAG, "onStart() called")
-    }
-
-    override fun onResume() {
-        super.onResume()
-        Log.d(TAG, "onResume() called")
-    }
-
-    override fun onPause() {
-        super.onPause()
-        Log.d(TAG, "onPause() called")
-    }
-
-    override fun onStop() {
-        super.onStop()
-        Log.d(TAG, "onStop() called")
-    }
-
-    override fun onDestroy() {
-        super.onDestroy()
-        Log.d(TAG, "onDestroy() called")
-    }
-
     private fun updateQuestion() {
-        val questionTextResId = questionBank[currentIndex].textResId
-        binding.questionTextView.setText(questionTextResId)
+        binding.questionTextView.setText(quizViewModel.currentQuestionText)
     }
 
     private fun checkAnswer(useAnswer: Boolean) {
-        val responseAnswer = questionBank[currentIndex].answer
         val contextView = findViewById<View>(android.R.id.content)
+        val responseAnswer = quizViewModel.currentQuestionAnswer
 
         val messageId =
             if (responseAnswer == useAnswer) {
-                userCorrectAnswer["Question: $currentIndex"] = "Response: $useAnswer"
+                quizViewModel.setCorrectAnswer(
+                    "Question: ${quizViewModel.getCurrentIndex}",
+                    "Response: $useAnswer"
+                )
                 R.string.correct_response
             } else R.string.incorrect_response
 
         Snackbar.make(contextView, messageId, Snackbar.LENGTH_SHORT).show()
+    }
 
-        if (currentIndex + 1 == questionBank.size) {
-            Snackbar.make(contextView, userPercentageCorrect(), Snackbar.LENGTH_INDEFINITE)
-                .setAction(R.string.Exit_button) {
-                    currentIndex = 0
-                    updateQuestion()
-                    enableButtons()
-                }
-                .show()
+    private fun disableOrEnableButton(
+        buttonsResponseAnswer: Boolean,
+        nextAndPrevButtons: Boolean = true
+    ) {
+        with(binding) {
+            falseButton.isEnabled = buttonsResponseAnswer
+            trueButton.isEnabled = buttonsResponseAnswer
+            nextButton.isEnabled = nextAndPrevButtons
+            previousButton.isEnabled = nextAndPrevButtons
         }
-    }
-
-    private fun disableButtons() {
-        binding.falseButton.isEnabled = false
-        binding.trueButton.isEnabled = false
-    }
-
-    private fun enableButtons() {
-        binding.falseButton.isEnabled = true
-        binding.trueButton.isEnabled = true
-    }
-
-    private fun userPercentageCorrect(): String {
-        val percentage = (userCorrectAnswer.size * 100) / currentIndex
-
-        return "Congratulations! You got $percentage% of the questions correct"
     }
 }
--- app/src/main/java/com/example/geoquiz/QuizViewModel.kt
+++ app/src/main/java/com/example/geoquiz/QuizViewModel.kt
@@ -1,19 +1,52 @@
 package com.example.geoquiz
 
-import android.util.Log
 import androidx.lifecycle.ViewModel
 
-private const val TAG = "QuizViewModel"
+private const val MAX_PERCENT = 100
 
-class QuizViewModel: ViewModel() {
+class QuizViewModel : ViewModel() {
+    private val questionBank = listOf(
+        Question(R.string.question_australia, true),
+        Question(R.string.question_oceans, true),
+        Question(R.string.question_mideast, false),
+        Question(R.string.question_africa, false),
+        Question(R.string.question_americas, true),
+        Question(R.string.question_asia, true)
+    )
 
-    init {
-        Log.d(TAG, "ViewModel instance is created")
+    private var currentIndex = 0
+
+    private val userCorrectAnswer = mutableMapOf<String, String>()
+
+    val getCurrentIndex: Int get() = currentIndex
+
+    val currentQuestionAnswer: Boolean
+        get() = questionBank[currentIndex].answer
+
+    val currentQuestionText: Int
+        get() = questionBank[currentIndex].textResId
+
+    val lastQuestion: Boolean get() = currentIndex + 1 == questionBank.size
+
+    fun moveToNext() {
+        currentIndex = (currentIndex + 1) % questionBank.size
     }
 
-    override fun onCleared() {
-        super.onCleared()
-        Log.d(TAG, "ViewModel instance about to be destroyed")
+    fun previousQuestion() {
+        currentIndex = (currentIndex - 1) % questionBank.size
     }
 
+    fun setCorrectAnswer(key: String, value: String) {
+        userCorrectAnswer[key] = value
+    }
+
+    fun resetCurrentIndex() {
+        currentIndex = 0
+    }
+
+    fun userPercentageCorrect(): String {
+        val percentage = (userCorrectAnswer.size * MAX_PERCENT) / currentIndex
+
+        return "Congratulations! You got $percentage% of the questions correct"
+    }
 }
