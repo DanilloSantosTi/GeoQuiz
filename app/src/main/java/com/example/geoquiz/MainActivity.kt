e056fab35240eb638755468d2768b3b06694b8d5
DanilloSantosTi <danillomdsti@gmail.com>
2023-04-05 00:12:06 -0300

    [master] feature - add CheatActivity

--- app/src/main/java/com/example/geoquiz/MainActivity.kt
+++ app/src/main/java/com/example/geoquiz/MainActivity.kt
@@ -1,7 +1,9 @@
 package com.example.geoquiz
 
+import android.app.Activity
 import android.os.Bundle
 import android.view.View
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import com.example.geoquiz.databinding.ActivityMainBinding
@@ -11,12 +13,27 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
     private val quizViewModel: QuizViewModel by viewModels()
+    private val cheatLauncher = registerForActivityResult(
+        ActivityResultContracts.StartActivityForResult()
+    ) { result ->
+        if (result.resultCode == Activity.RESULT_OK) {
+            quizViewModel.isCheater =
+                result.data?.getBooleanExtra(EXTRA_ANSWER_SHOWN, false)
+                    ?: false
+        }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
         updateQuestion()
+
+        binding.cheatButton.setOnClickListener {
+            val answerIsTrue = quizViewModel.currentQuestionAnswer
+            val intent = CheatActivity.newIntent(this@MainActivity, answerIsTrue)
+            cheatLauncher.launch(intent)
+        }
 
         binding.trueButton.setOnClickListener {
             checkAnswer(true)
@@ -40,6 +57,7 @@ class MainActivity : AppCompatActivity() {
                 )
                     .setAction(R.string.Exit_button) {
                         quizViewModel.resetCurrentIndex()
+                        quizViewModel.clearData()
                         updateQuestion()
                         disableOrEnableButton(
                             buttonsResponseAnswer = true,
@@ -65,12 +83,16 @@ class MainActivity : AppCompatActivity() {
 
     private fun updateQuestion() {
         binding.questionTextView.setText(quizViewModel.currentQuestionText)
+
+        quizViewModel.isCheater = quizViewModel.getAnswerCheater()
+
     }
 
     private fun checkAnswer(useAnswer: Boolean) {
         val contextView = findViewById<View>(android.R.id.content)
 
-        Snackbar.make(contextView, quizViewModel.checkAnswer(useAnswer), Snackbar.LENGTH_SHORT).show()
+        Snackbar.make(contextView, quizViewModel.checkAnswer(useAnswer), Snackbar.LENGTH_SHORT)
+            .show()
     }
 
     private fun disableOrEnableButton(
--- app/src/main/java/com/example/geoquiz/QuizViewModel.kt
+++ app/src/main/java/com/example/geoquiz/QuizViewModel.kt
@@ -1,10 +1,12 @@
 package com.example.geoquiz
 
+import android.util.Log
 import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
 
 private const val MAX_PERCENT = 100
 const val CURRENT_INDEX_KEY = "CURRENT_INDEX_KEY"
+const val IS_CHEATER_KEY = "IS_CHEATER_KEY"
 
 class QuizViewModel(private val savedStateHandle: SavedStateHandle) : ViewModel() {
     private val questionBank = listOf(
@@ -16,12 +18,17 @@ class QuizViewModel(private val savedStateHandle: SavedStateHandle) : ViewModel(
         Question(R.string.question_asia, true)
     )
 
+    var isCheater: Boolean
+        get() = savedStateHandle.get(IS_CHEATER_KEY) ?: false
+        set(value) = savedStateHandle.set(IS_CHEATER_KEY, value)
+
     private var currentIndex: Int
         get() = savedStateHandle[CURRENT_INDEX_KEY] ?: 0
         set(value) = savedStateHandle.set(CURRENT_INDEX_KEY, value)
 
 
-    private val userCorrectAnswer = mutableMapOf<String, String>()
+    private val userCorrectAnswer = mutableListOf<UserResponseQuestions>()
+    private val userAnswerCheat = mutableMapOf<Int, Boolean>()
 
     val getCurrentIndex: Int get() = currentIndex
 
@@ -31,7 +38,7 @@ class QuizViewModel(private val savedStateHandle: SavedStateHandle) : ViewModel(
     val currentQuestionText: Int
         get() = questionBank[currentIndex].textResId
 
-    val lastQuestion: Boolean get() = currentIndex + 1 == questionBank.size
+    val lastQuestion: Boolean get() = getCurrentIndex + 1 == questionBank.size
 
     fun moveToNext() {
         currentIndex = (currentIndex + 1) % questionBank.size
@@ -41,29 +48,51 @@ class QuizViewModel(private val savedStateHandle: SavedStateHandle) : ViewModel(
         currentIndex = (currentIndex - 1) % questionBank.size
     }
 
-    fun setCorrectAnswer(key: String, value: String) {
-        userCorrectAnswer[key] = value
+    fun setCorrectAnswer(question: String, response: String) {
+        userCorrectAnswer.add(
+            UserResponseQuestions(
+                question = question,
+                userResponse = response,
+            )
+        )
     }
 
     fun resetCurrentIndex() {
         currentIndex = 0
     }
 
+    fun clearData(){
+        userCorrectAnswer.clear()
+    }
+
     fun userPercentageCorrect(): String {
-        val percentage = (userCorrectAnswer.size * MAX_PERCENT) / currentIndex
+        val percentage = (userCorrectAnswer.size * MAX_PERCENT) / questionBank.size
+        Log.d("size userCorrectAnswer", "${userCorrectAnswer.size}")
+        Log.d("size questionBank", "${questionBank.size}")
 
         return "Congratulations! You got $percentage% of the questions correct"
+    }
+
+    fun getAnswerCheater(): Boolean {
+        return userAnswerCheat[getCurrentIndex] == true
     }
 
     fun checkAnswer(useAnswer: Boolean): Int {
         val responseAnswer = currentQuestionAnswer
 
-        return if (responseAnswer == useAnswer) {
+        if (responseAnswer == useAnswer) {
             setCorrectAnswer(
                 "Question: $getCurrentIndex",
-                "Response: $useAnswer"
+                "Response: $useAnswer",
             )
-            R.string.correct_response
-        } else R.string.incorrect_response
+        }
+
+        userAnswerCheat[getCurrentIndex] = isCheater
+
+        return when {
+            isCheater -> R.string.judgment_toast
+            responseAnswer == useAnswer -> R.string.correct_response
+            else -> R.string.incorrect_response
+        }
     }
 }
